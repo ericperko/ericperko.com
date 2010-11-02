b1e30ecfdb7e469d94d374da45d20e3642bf23b4
Stephen Johnson <srj15@case.edu>
2010-11-02 15:12:39 -0400

    Removed lots of redundancy in views

--- hipflask/site.py
+++ hipflask/site.py
@@ -1,4 +1,5 @@
 from __future__ import with_statement
+import collections
 import logging
 import yaml
 import copy
@@ -7,7 +8,7 @@ from flask import url_for, render_template
 apps = {}
 reverse_apps = {}
 groups = []
-paged_apps = {}
+page_counts = collections.defaultdict(lambda: 1)
 
 with open('content/site.yaml', 'r') as f:
     yaml_repr = yaml.load(f)
@@ -21,15 +22,14 @@ for group_repr in yaml_repr:
             num_pages = item['pages']
             del item['pages']
         else:
-            num_pages = 0
+            num_pages = 1
         item_name = item.keys()[0]
         item_url = item.values()[0]
         items.append({'name': item_name, 'url': item_url})
         apps[item_name] = item_url
         reverse_apps[item_url] = item_name
-        
-        if num_pages > 0:
-            paged_apps[item_url] = num_pages
+        page_counts[item_url] = num_pages
+    
     groups.append({
         'name': group_name,
         'items': items
@@ -41,7 +41,6 @@ def parse_groups():
         for item in group['items']:
             if not item['url'].startswith('http'):
                 item['url'] = url_for(item['url'])
-            logging.info(str(item))
     return parsed_groups
 
 def render_with_app_lists(template, **kwargs):
--- hipflask/views.py
+++ hipflask/views.py
@@ -10,7 +10,7 @@ from hipflask import app
 from flask import render_template, flash, url_for, redirect, Markup, abort
 
 import web2point0
-from site import render_with_app_lists, reverse_apps, paged_apps
+from site import *
 
 # ==============
 # = Front page =
@@ -37,33 +37,26 @@ def run_updates():
 # = Pages =
 # =========
 
-def apps(app_name):
-    if app_name in paged_apps.keys():
-        return paged_app(app_name, page=1)
-    with open('content/%s.markdown' % app_name, 'r') as f:
-        html = Markup(markdown.markdown(f.read()))
-    context = {
-        'title': reverse_apps[app_name],
-        'html': html
-    }
-    return render_with_app_lists('app.html', **context)
-
 @app.route('/<app_name>/<int:page>')
-def paged_app(app_name, page=1):
-    total_pages = paged_apps[app_name]
+def apps(app_name, page=1):
+    context = {'title': reverse_apps[app_name]}
+    total_pages = page_counts[app_name]
     if page > total_pages:
         abort(404)
-    pages = range(1, int(total_pages)+1)
-    this_page=int(page)
-    context = {'title': reverse_apps[app_name]}
-    if this_page > 1:
-        context['previous_page'] = url_for('paged_app', app_name=app_name, page=this_page-1)
-    if this_page < total_pages:
-        context['next_page'] = url_for('paged_app', app_name=app_name, page=this_page+1)
+    if total_pages == 1:
+        template_name = 'content/%s.markdown' % app_name
+    else:
+        pages = range(1, int(total_pages)+1)
+        this_page=int(page)
+        if this_page > 1:
+            context['previous_page'] = url_for('paged_app', app_name=app_name, page=this_page-1)
+        if this_page < total_pages:
+            context['next_page'] = url_for('paged_app', app_name=app_name, page=this_page+1)
+        template_name = 'content/%s_%s.markdown' % (app_name, page)
     
-    with open('content/%s_%s.markdown' % (app_name, page)) as f:
+    with open(template_name, 'r') as f:
         context['html'] = Markup(markdown.markdown(f.read()))
-    return render_with_app_lists('multipage.html', **context)
+    return render_with_app_lists('page.html', **context)
 
 for item in reverse_apps.keys():
     app.add_url_rule('/%s' % item, item, functools.partial(apps, item))
