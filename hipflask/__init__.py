0a4633d9f56f867b6435b5fe27838544cef14298
Stephen Johnson <srj15@case.edu>
2010-11-03 14:52:19 -0400

    Added default settings, you can optionally put settings in /settings.py

--- hipflask/__init__.py
+++ hipflask/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 import settings
 
 app = Flask('hipflask')
-app.config.from_object('hipflask.settings')
+app.config.from_object('hipflask.default_settings')
+app.config.from_object('settings')
 
 import site
 import views
