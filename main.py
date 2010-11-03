8360cd71bbc3cd8102a39d6c406dca7d3f701e7c
Stephen Johnson <srj15@case.edu>
2010-11-03 00:42:00 -0400

    Moved site-specific templates (i.e. all of them) into content/templates

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from google.appengine.ext.webapp.util import run_wsgi_app
+from jinja2 import FileSystemLoader
+import os
 from hipflask import app
+
+if len(app.jinja_loader.searchpath) == 1:
+    # Add more options for loading templates, since we don't
+    # actually want templates to be stored in hipflask/ but
+    # rather in content/
+    import os
+    app.jinja_loader.searchpath.append(os.path.join(app.root_path, '..', 'content', 'templates'))
 
 run_wsgi_app(app)
