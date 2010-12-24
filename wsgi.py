5a6a48b660720c930078bd7872fe6090763aaa6f
Eric Perko <wisesage5001@gmail.com>
2010-12-23 21:41:27 -0500

    Slight change for Apache

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,13 @@
 from jinja2 import FileSystemLoader
 import os
-from hipflask import app
+from hipflask import app as application
 
-if len(app.jinja_loader.searchpath) == 1:
+if len(application.jinja_loader.searchpath) == 1:
     # Add more options for loading templates, since we don't
     # actually want templates to be stored in hipflask/ but
     # rather in content/
     import os
-    app.jinja_loader.searchpath.append(os.path.join(app.root_path, '..', 'content', 'templates'))
+    application.jinja_loader.searchpath.append(os.path.join(application.root_path, '..', 'content', 'templates'))
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
